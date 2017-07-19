a3b93dc818d97ad3e846b8a4a88903e0b01b91d0
Reilly Bergin-Pugh <rberginpugh@gmail.com>
2017-07-18 20:08:21 -0400

    used devise to add fb authentication for the user login

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'coffee-rails', '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 gem 'devise'
+gem 'omniauth-facebook'
 # Use jquery as the JavaScript library
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
 gem 'turbolinks', '~> 5'
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, controllers: {
         registrations: 'users/registrations',
-        sessions: 'users/sessions'
-      }
+        sessions: 'users/sessions',
+        omniauth_callbacks: "users/omniauth_callbacks" 
+      }  
   root to: "dogs#index"
   get "/home", to: "dogs#index"
   resources :dogs
